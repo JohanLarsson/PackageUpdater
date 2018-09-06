62255c391a04805a929803c967de769227a4397e
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-06 09:38:19 +0200

    Capture standard error & set working dir.

--- PackageUpdater/PackageUpdate.cs
+++ PackageUpdater/PackageUpdate.cs
@@ -1,23 +1,28 @@
 ﻿namespace PackageUpdater
 {
     using System.ComponentModel;
-    using System.Diagnostics;
     using System.Runtime.CompilerServices;
     using System.Windows.Input;
 
     public class PackageUpdate : INotifyPropertyChanged
     {
-        private Process updateProcess;
-
         public PackageUpdate(Repository repository, string package)
         {
             this.Repository = repository;
-            this.Package = package;
-            this.UpdateCommand = new RelayCommand(
-                _ => this.Run(),
-                _ => this.UpdateProcess == null && !string.IsNullOrWhiteSpace(package));
+            this.UpdateProcess = new UpdateProcess(repository, package);
+
             this.DeleteDotVsFolderCommand = new RelayCommand(
-                _ => this.Repository.DotVsDirectory?.Delete(true),
+                _ =>
+                {
+                    try
+                    {
+                        this.Repository.DotVsDirectory?.Delete(true);
+                    }
+                    catch
+                    {
+                        // just swallowing
+                    }
+                },
                 _ => this.Repository.DotVsDirectory != null);
         }
 
@@ -25,36 +30,13 @@
 
         public Repository Repository { get; }
 
-        public string Package { get; }
-
-        public ICommand UpdateCommand { get; }
+        public UpdateProcess UpdateProcess { get; }
 
         public ICommand DeleteDotVsFolderCommand { get; }
-
-        public Process UpdateProcess
-        {
-            get => this.updateProcess;
-            private set
-            {
-                if (ReferenceEquals(value, this.updateProcess))
-                {
-                    return;
-                }
-
-                this.updateProcess = value;
-                this.OnPropertyChanged();
-            }
-        }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private void Run()
-        {
-            this.UpdateProcess = Process.Start(this.Repository.PaketExe.FullName, $"update {this.Package}");
-            this.updateProcess.OnExit(() => this.UpdateProcess = null);
         }
     }
 }
---
+++ PackageUpdater/UpdateProcess.cs
@@ -0,0 +1,67 @@
+﻿namespace PackageUpdater
+{
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Runtime.CompilerServices;
+    using System.Windows.Input;
+
+    public class UpdateProcess : INotifyPropertyChanged
+    {
+        private readonly Repository repository;
+        private string result;
+
+        public UpdateProcess(Repository repository, string package)
+        {
+            this.repository = repository;
+            this.UpdateCommand = new RelayCommand(
+                _ => this.Run(),
+                _ => this.result == null && !string.IsNullOrWhiteSpace(package));
+            this.Package = package;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand UpdateCommand { get; }
+
+        public string Package { get; }
+
+        public string Result
+        {
+            get => this.result;
+            set
+            {
+                if (value == this.result)
+                {
+                    return;
+                }
+
+                this.result = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Run()
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo(this.repository.PaketExe.FullName, $" update {this.Package}")
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    WorkingDirectory = this.repository.RootDirectory.FullName,
+                },
+            };
+         
+            process.Start();
+            this.Result = process.StandardOutput.ReadToEnd() + Environment.NewLine + process.StandardError.ReadToEnd();
+        }
+    }
+}
