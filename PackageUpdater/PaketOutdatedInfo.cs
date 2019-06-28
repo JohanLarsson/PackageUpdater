05e3d6ab0ddb9dea655181710b6f45d5960c74ba
Johan Larsson <johan_gavle1@hotmail.com>
2019-06-28 22:09:16 +0200

    Amend.

--- PackageUpdater/PaketOutdatedInfo.cs
+++ PackageUpdater/PaketOutdatedInfo.cs
@@ -3,56 +3,22 @@
     using System.ComponentModel;
     using System.Runtime.CompilerServices;
 
-    public class ReplacePackageInfo : ITaskInfo
+    public class PaketOutdatedInfo : ITaskInfo
     {
         private string oldPackageId;
         private string newPackageId;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public string OldPackageId
-        {
-            get => this.oldPackageId;
-            set
-            {
-                if (value == this.oldPackageId)
-                {
-                    return;
-                }
-
-                this.oldPackageId = value;
-                this.OnPropertyChanged();
-            }
-        }
-
-        public string NewPackageId
-        {
-            get => this.newPackageId;
-            set
-            {
-                if (value == this.newPackageId)
-                {
-                    return;
-                }
-
-                this.newPackageId = value;
-                this.OnPropertyChanged();
-            }
-        }
-
         public Batch CreateBatch(Repository repository)
         {
-            if (PaketReplace.TryCreate(repository, this.OldPackageId, this.NewPackageId, out var replace) &&
-                PaketInstall.TryCreate(repository, out var paketInstall))
+            if (PaketOutdated.TryCreate(repository,  out var outdated))
             {
                 return new Batch(
-                      new GitAssertEmptyDiff(repository.Directory),
-                      new GitAssertIsOnMaster(repository.Directory),
-                      new GitPullFastForwardOnly(repository.Directory),
-                      new GitCleanDxf(repository.Directory),
-                      replace,
-                      paketInstall,
-                      new DotnetRestore(repository.Directory));
+                    new GitAssertEmptyDiff(repository.Directory),
+                    new GitPullFastForwardOnly(repository.Directory),
+                    new GitCleanDxf(repository.Directory),
+                    outdated);
             }
             else
             {
--- PackageUpdater/TaskListViewModel.cs
+++ PackageUpdater/TaskListViewModel.cs
@@ -33,7 +33,7 @@
 
         public IReadOnlyView<TaskViewModel> Tasks { get; }
 
-        public ITaskInfo TaskInfo { get; } = new UpdatePackageInfo();
+        public ITaskInfo TaskInfo { get; } = new PaketOutdatedInfo();
 
         public TaskViewModel SelectedTask
         {
